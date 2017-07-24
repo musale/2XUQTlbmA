a4c4ff619d6efdcd539bb59c786ab1a65db5241d
musale <martinmshale@gmail.com>
2017-07-24 18:11:53 +0300

    Add login, logout and signup for a user.

--- talky/core/urls.py
+++ talky/core/urls.py
@@ -1,8 +1,11 @@
 """Create the URLs to be used by the talky project."""
 from django.conf.urls import url
 
-from core.views import user_list
+from core import views
 
 urlpatterns = [
-    url(r'^$', user_list, name='user_list'),
+    url(r'^$', views.user_list, name='user_list'),
+    url(r'^log_in/$', views.log_in, name='log_in'),
+    url(r'^log_out/$', views.log_out, name='log_out'),
+    url(r'^sign_up/$', views.sign_up, name='sign_up'),
 ]
--- talky/core/views.py
+++ talky/core/views.py
@@ -1,7 +1,42 @@
 """Render the templates."""
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, render
 
 
 def user_list(request):
     """Render the user list template."""
     return render(request, 'core/user_list.html')
+
+
+def log_in(request):
+    """Authenticate a user for login."""
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(reverse('core:user_list'))
+        else:
+            print(form.errors)
+    return render(request, 'core/login.html', {'form': form})
+
+
+def log_out(request):
+    """Log out a user."""
+    logout(request)
+    return redirect(reverse('core:log_in'))
+
+
+def sign_up(request):
+    """Create a new user."""
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('core:log_in'))
+        else:
+            print(form.errors)
+    return render(request, 'core/signup.html', {'form': form})
