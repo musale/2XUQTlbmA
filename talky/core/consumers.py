8e26647580738bb003492fbce9a2aad3ac5bc1f8
musale <martinmshale@gmail.com>
2017-07-25 12:03:43 +0300

    Update(consumers.py): add flags in the user group for logged in/out users

--- talky/core/consumers.py
+++ talky/core/consumers.py
@@ -1,12 +1,29 @@
 """Handle basic connection between client and server."""
+import json
+
 from channels import Group
+from channels.auth import channel_session_user, channel_session_user_from_http
 
 
+@channel_session_user_from_http
 def ws_connect(message):
     """Add a connected user to the 'users' group."""
     Group('users').add(message.reply_channel)
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': True
+        })
+    })
 
 
+@channel_session_user
 def ws_disconnect(message):
     """Remove a disconnected user from the 'user' group."""
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': False
+        })
+    })
     Group('users').discard(message.reply_channel)
