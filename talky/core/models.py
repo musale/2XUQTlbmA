7760f72a5e5a03fa1f7587ba5120049c43975416
musale <martinmshale@gmail.com>
2017-07-25 12:04:18 +0300

    Update(models.py): add a model to track logged in users.

--- talky/core/models.py
+++ talky/core/models.py
@@ -1,5 +1,12 @@
+"""Core application models."""
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class LoggedInUser(models.Model):
+    """Track a user login activities."""
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='logged_in_user')
