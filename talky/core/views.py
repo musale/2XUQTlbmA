b30f60d1a8062666134ce41b7b5e2b84ec130433
musale <martinmshale@gmail.com>
2017-07-25 12:05:24 +0300

    Update(views.py): list all users with statuses and use authentication rules

--- talky/core/views.py
+++ talky/core/views.py
@@ -1,13 +1,21 @@
 """Render the templates."""
-from django.contrib.auth import login, logout
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
 
+User = get_user_model()
 
+
+@login_required(login_url='/log_in/')
 def user_list(request):
     """Render the user list template."""
-    return render(request, 'core/user_list.html')
+    users = User.objects.select_related('logged_in_user')
+    for user in users:
+        user.status = 'Online' if hasattr(
+            user, 'logged_in_user') else 'Offline'
+    return render(request, 'core/user_list.html', {'users': users})
 
 
 def log_in(request):
@@ -23,6 +31,7 @@ def log_in(request):
     return render(request, 'core/login.html', {'form': form})
 
 
+@login_required(login_url='/log_in/')
 def log_out(request):
     """Log out a user."""
     logout(request)
